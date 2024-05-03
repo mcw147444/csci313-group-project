de38c4a44250f21abe0a9dc8e99fca0f391aa4d2
alexanderHaskins <alexjhaskins@yahoo.com>
2024-05-03 16:52:18 -0500

    Got everything except the template done
    The error was cause because I used catalog.catalog.html instead of catalog/catalog.html. I also moved away from using the generic list view in favor of another method

--- GroupAssignment/catalog/urls.py
+++ GroupAssignment/catalog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
-    path('books/', views.BookListView.as_view(), name='books'),
+    path('catalog/',views.catalog,name='catalog')
 ]
--- GroupAssignment/catalog/views.py
+++ GroupAssignment/catalog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.views import generic
 from .models import Book
 
 # Create your views here.
 def home(request):
     context={}
     return render(request, 'index.html',context=context)
-class BookListView(generic.ListView):
-    model = Book
+def catalog(request):
+    book_list=Book.objects.all()
+    book_dictionary={'book':book_list}
+    return render(request,'catalog/catalog.html',context=book_dictionary)
