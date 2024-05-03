0f50fb1703c9e0a63d2114781b0a1dce0c81125a
alexanderHaskins <alexjhaskins@yahoo.com>
2024-05-03 17:47:51 -0500

    Migrations and admin registration
    Migrated the models and registered the admin page

--- GroupAssignment/catalog/admin.py
+++ GroupAssignment/catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from catalog.models import Book,Author,Book_Instance,Genre,Language
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Book_Instance)
+admin.site.register(Genre)
+admin.site.register(Language)
