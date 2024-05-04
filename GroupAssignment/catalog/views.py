7d6dbcd9839f7c2585d49af3b7065b94251a0d4d
alexanderHaskins <alexjhaskins@yahoo.com>
2024-05-04 18:42:37 -0500

    Set up loaned page
    Set up a loaned page to see which books the current user has checked out, I just need to make a link to the page

--- GroupAssignment/catalog/views.py
+++ GroupAssignment/catalog/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book,Book_Instance
 from django.views import generic
 
 # Create your views here.
 def index(request):
     context={}
     return render(request, 'index.html',context=context)
+
+def LoanedBooksView(request):
+    context={}
+    instance_list=Book_Instance.objects.filter(borrower=request.user)
+    instance_dictionary={'instance':instance_list}
+    return render(request,'catalog/loaned_books.html',context=instance_dictionary)
+
 
 class BookListView(generic.ListView):
     model = Book
